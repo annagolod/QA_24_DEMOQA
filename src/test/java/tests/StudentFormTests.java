37d45aee48cd41ba9b1a660043575e640b66504a
annagolod <anna.golod@gmail.com>
2024-03-12 19:51:00 +0200

    added AppManager

---
+++ src/test/java/tests/StudentFormTests.java
@@ -0,0 +1,14 @@
+package tests;
+
+
+import manager.HelperStudent;
+import org.testng.annotations.Test;
+
+public class StudentFormTests extends TestBase implements HelperStudent {
+
+    @Test
+
+    public void studentFormPositiveTest(){
+
+    }
+}
